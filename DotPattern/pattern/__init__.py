ff43bfdcb3c2c80a93f5270c6b2a7bce8308e210
KlogeBukser <111913719+moyasui@users.noreply.github.com>
2024-08-01 16:16:41 +0100

    Working one person game

--- DotPattern/pattern/__init__.py
+++ DotPattern/pattern/__init__.py
@@ -1,10 +1,10 @@
 from otree.api import *
 from pattern.pattern_utils import get_pattern, compare_patterns
+import numpy as np
 
 doc = """
 Your app description
 """
-
 
 class C(BaseConstants):
     NAME_IN_URL = 'pattern'
@@ -23,36 +23,74 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    reproduced_pattern = models.StringField()  # To store the pattern as a string
+
+
+# FUNCTIONS
+def set_random_pattern(player):
+    ''' Store the random pattern in a list.'''
+    random_pattern = get_pattern(C.PATTERN_SIZE, C.N_DOTS)
+    print(random_pattern.tolist())
+    player.session.vars['random_pattern'] = random_pattern#.tolist()
+    return random_pattern#.tolist()
+
+
+def store_reproduced_pattern(player):
+    # Convert the reproduced pattern string into a matrix
+    reproduced_pattern = player.reproduced_pattern.split(',')
+    reproduced_pattern = [int(i) for i in reproduced_pattern]
+    original_pattern = player.session.vars.get('random_pattern', [])
+    reproduced_pattern = np.array(reproduced_pattern).reshape(original_pattern.shape)
+    return reproduced_pattern, original_pattern
+
+
+def get_comparison_results(player):
+    original_pattern = player.session.vars.get('random_pattern', [])
+    reproduced_pattern, _ = store_reproduced_pattern(player)
+    correctness = compare_patterns(original_pattern, reproduced_pattern)
+    return correctness, original_pattern, reproduced_pattern
 
 
 # PAGES
+class Instructions(Page):
+    pass 
+
 class PatternDisplay(Page):
-    
-    form_model = 'player'
-    # form_fields = ['pattern']
-
-    def vars_for_template(self):
-        random_pattern = get_pattern(C.PATTERN_SIZE, C.N_DOTS)
+    @staticmethod
+    def vars_for_template(player):
+        matrix = set_random_pattern(player)
         return {
-            'matrix': random_pattern.tolist(),
+            'matrix': matrix.tolist(),
         }
-
-    # timeout stuff
 
 class Reproduce(Page):
     form_model = 'player'
-    # form_fields = ['pattern']
+    form_fields = ['reproduced_pattern']
 
-    def vars_for_template(self):
-        return 
-    
+    @staticmethod
+    def vars_for_template(player):
+        size = C.PATTERN_SIZE
+        return {
+            'pattern_size': range(size),
+            'size_value': size,
+            'n_dots': C.N_DOTS
+        }
 
-class ResultsWaitPage(WaitPage):
-    pass
-
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        reproduced_pattern = player.reproduced_pattern.split(',')
+        # selected_dots = sum(1 for dot in reproduced_pattern if dot == '1')
+       
 
 class Results(Page):
-    pass
+    @staticmethod
+    def vars_for_template(player):
+        correctness, original_pattern, reproduced_pattern = get_comparison_results(player)
+        return {
+            'accuracy': correctness,
+            'original_pattern': original_pattern.tolist(),
+            'reproduced_pattern': reproduced_pattern.tolist()
+        }
 
-page_sequence = [PatternDisplay, Reproduce, ResultsWaitPage, Results]
+
+page_sequence = [Instructions, PatternDisplay, Reproduce, Results]
--- DotPattern/pattern/pattern_utils.py
+++ DotPattern/pattern/pattern_utils.py
@@ -8,22 +8,32 @@ def get_pattern(size, n_dots):
 
 	chosen_numbers = random.sample(grid_numbers, n_dots)
 	for i in chosen_numbers:
-		x, y = i // 10, i % 10
+		x, y = i // size, i % size
 		seed_pattern[x, y] = 1
 		  
 	return seed_pattern
 
 def compare_patterns(A, B):
+	A = np.array(A)
+	B = np.array(B).reshape(A.shape)
 	'''Compares two matrices and returns the percentage of correctly matched figure'''
 	if A.shape != B.shape:
 		raise ValueError('Matrices must be of the same shape')
 
-	return np.sum(A == B)/A.size
+	n_dots = np.sum(A) 
+	n_correct = 0
+	for i in range(A.shape[0]):
+		for j in range(A.shape[1]):
+			if A[i,j]:
+				if A[i, j] == B[i, j]:
+					n_correct += 1
+
+	return n_correct / n_dots
 
 
 if __name__ == '__main__':
-	A = get_pattern(10)
-	B = get_pattern(10)
+	A = get_pattern(10, 12)
+	B = get_pattern(10, 12)
 
 	print(A)
 	print(B)
